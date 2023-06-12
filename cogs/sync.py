ab8cdf83c1b3644311e13569529458d857687db9
ed.xyz <eddietang2314@gmail.com>
2023-06-12 16:58:07 -0400

    GIOIADSFTTY

---
+++ cogs/sync.py
@@ -0,0 +1,52 @@
+from typing import Literal, Optional
+import discord
+from discord.ext.commands import Greedy, Context
+from discord.ext import commands
+
+
+class WaifuSync(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        return
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.is_owner()
+    async def sync(
+        self,
+        ctx: Context,
+        guilds: Greedy[discord.Object],
+        spec: Optional[Literal["~", "*", "^"]] = None,
+    ) -> None:
+        if not guilds:
+            if spec == "~":
+                synced = await ctx.bot.tree.sync(guild=ctx.guild)
+            elif spec == "*":
+                ctx.bot.tree.copy_global_to(guild=ctx.guild)
+                synced = await ctx.bot.tree.sync(guild=ctx.guild)
+            elif spec == "^":
+                ctx.bot.tree.clear_commands(guild=ctx.guild)
+                await ctx.bot.tree.sync(guild=ctx.guild)
+                synced = []
+            else:
+                synced = await ctx.bot.tree.sync()
+
+            await ctx.send(
+                f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+            )
+            return
+
+        ret = 0
+        for guild in guilds:
+            try:
+                await ctx.bot.tree.sync(guild=guild)
+            except discord.HTTPException:
+                pass
+            else:
+                ret += 1
+
+        await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
+
+async def setup(bot):
+    await bot.add_cog(WaifuSync(bot))
--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import asyncio
+from typing import Any, Coroutine
 import discord
-from discord.ext import commands, tasks
+from discord.ext.commands import Context
+from discord.ext import commands
+from discord.ext.commands.context import Context
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-token = os.getenv("TOKEN")
+
+TEST = 958488149724635146
 
 
-class Waifu(discord.Client):
+class Waifu(commands.Bot):
     def __init__(self):
-        super().__init__(intents=discord.Intents.all())
+        super().__init__(intents=discord.Intents.all(), command_prefix="waifu.")
         self.game = discord.Game("selfie.io")
 
     async def on_ready(self):
-        await client.change_presence(status=discord.Status.idle, activity=self.game)
+        await self.change_presence(status=discord.Status.idle, activity=self.game)
+
         print("Logged on as", self.user)
+        print(bot.user.id)
 
     async def on_message(self, msg: discord.Message):
+        await self.process_commands(msg)
         if msg.author.bot:
             return
 
@@ -26,14 +33,34 @@ class Waifu(discord.Client):
             return
         await msg.channel.send("hi")
 
-    # async def on_message(self, message):
-    #     # don't respond to ourselves
-    #     if message.author == self.user:
-    #         return
+        content = str(msg.content).strip(f"<@{self.user.id}>")
 
-    #     if message.content == "ping":
-    #         await message.channel.send("pong")
+        print(content)
+
+    async def on_command_error(self, context: Context, exception):
+        print(context.author)
+        print(exception)
+        return await super().on_command_error(context, exception)
 
 
-client = Waifu()
-client.run(token)
+async def load_extensions(bot):
+    await bot.load_extension(f"cogs.sync")
+
+
+bot = Waifu()
+
+
+@bot.tree.command()
+async def help(interaction: discord.Interaction):
+    """Help"""  # Description when viewing / commands
+    await interaction.response.send_message("hello")
+
+
+async def main():
+    async with bot:
+        await load_extensions(bot)
+        await bot.start(os.getenv("TOKEN"))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
